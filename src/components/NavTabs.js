93b77e7d1e213052e5b54c4972c5b83aa7e2877d
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-27 15:58:43 -0400

    added code to Home.js and index.css

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,44 +3,50 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#projects"
-          onClick={() => handlePageChange('Projects')}
-          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-        >
-          Projects
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div className='topbar'>
+        <ul className="nav nav-tabs">
+        <li className="nav-item">
+            <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            >
+            Home
+            </a>
+        </li>
+        <li className="nav-item">
+            <a
+            href="#about"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+            >
+            About
+            </a>
+        </li>
+        <li className="nav-item">
+            <a
+            href="#projects"
+            onClick={() => handlePageChange('Projects')}
+            className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+            >
+            Projects
+            </a>
+        </li>
+        <li className="nav-item">
+            <a
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
+            Contact
+            </a>
+        </li>
+        </ul>
+        <ul className='logo-section'>
+        <li>Mark Barstow</li>
+        </ul>        
+    </div>
+
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,19 +2,16 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
+    <div className='home-page'>
       <h1>Home Page</h1>
+      <img src="../../assets/images/homepage.jpg"></img>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+        I am a 21 y/o bootcamp student from Charlottesville, Virginia 
+        who is looking to enhance my creative ability through code. 
+        In addition to development, my passions lie in the subjects of art, 
+        design, and music production. It is my goal to be able to find a profession 
+        that merges my intellectual and skill-based pursuits with the ability to 
+        creatively display my talent.
       </p>
     </div>
   );

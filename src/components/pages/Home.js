584e35af0974d5baed7b8413a1499c229d360568
marchetype <114883447+marchetype@users.noreply.github.com>
2023-03-27 16:28:26 -0400

    established font styles from Google Fonts. added background image to Homepage.

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import backgroundImage from '../../assets/images/homepage.jpg';
 
 export default function Home() {
   return (
     <div className='home-page'>
-      <h1>Home Page</h1>
-      <img src="../../assets/images/homepage.jpg"></img>
-      <p>
-        I am a 21 y/o bootcamp student from Charlottesville, Virginia 
-        who is looking to enhance my creative ability through code. 
-        In addition to development, my passions lie in the subjects of art, 
-        design, and music production. It is my goal to be able to find a profession 
-        that merges my intellectual and skill-based pursuits with the ability to 
-        creatively display my talent.
-      </p>
+        <div className='home-image'>
+            <img src={backgroundImage} alt='homepage'></img>
+        </div>
+        <h1>MARK BARSTOW</h1>
+        <p>
+            I am a 21 y/o bootcamp student from Charlottesville, Virginia 
+            who is looking to enhance my creative ability through code. 
+            In addition to development, my passions lie in the subjects of art, 
+            design, and music production. It is my goal to be able to find a profession 
+            that merges my intellectual and skill-based pursuits with the ability to 
+            creatively display my talent.
+        </p>
     </div>
   );
 }
